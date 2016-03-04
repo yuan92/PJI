7b8552c801f0d8daeec0bd00054371babf315143
yuan92 <yuan140218@gmail.com>
2016-03-05 03:06:25 +0800

    Delete conn.php

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$mysql_server_name='localhost';
- 	$mysql_username='root'; 
-	$mysql_password='';
-	$mysql_database='test'; //le nom de base de donnee
-	$conn = @mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("il y a une erreur");
-	mysql_select_db($mysql_database); //ouverir le base de donnee
-?>
