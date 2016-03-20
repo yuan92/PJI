ace51914e50fd214691db574627d58de2d95ea51
yuan92 <yuan140218@gmail.com>
2016-03-20 23:53:01 +0100

    un petit probleme de condition de recherche

--- search.php
+++ search.php
@@ -10,23 +10,44 @@
 	.box h1{padding-left:10px;height:22px;line-height:22px;background:#f1f1f1;font-weight:bold;}
 	.box{position:relative;border:1px solid #e7e7e7;}
 	</style>
-	<script type="text/javascript">
-	$(document).ready(function(){
-	   $(".box h1").toggle(function(){
-	     $(this).next(".text").animate({height: 'toggle', opacity: 'toggle'}, "slow");
-	   },function(){
-	   $(this).next(".text").animate({height: 'toggle', opacity: 'toggle'}, "slow");
-	   });
-	});
+	<script>
+	function show(c_Str){
+		if(document.all(c_Str).style.display=='none'){
+			document.all(c_Str).style.display='block';
+		}
+		else{
+			document.all(c_Str).style.display='none';}
+		}
+	function high(){
+	if (event.srcElement.className=="k"){
+		event.srcElement.style.background="336699"
+		event.srcElement.style.color="white"
+		}
+	}
+	function low(){
+	if (event.srcElement.className=="k"){
+		event.srcElement.style.background="99CCFF"
+		event.srcElement.style.color=""
+		}
+	}
 	</script>
 </head>
 <body>
 
 	<a href='index.php' target='_self'>Retourne</a>
-	<div>
+	<div>	
+	<h1><div class=up onclick=show("a")>$prenom</h1><div onmouseover=high() onmouseout=low() id=a style="display:none">		
+	<table border="1" width="400">
+			  <tr>
+			    <th width="150">N</th>
+			    <th> prenom</th> 
+			    <th class=k>sexe</th>
+				<th class=k>choisir</th>
+			  </tr>
 		<?php
 			//还缺少对0条记录的处理-_-
 			//对搜索条件的性别进行处理
+	    	require_once("lib/conn.php");
 			if(empty($_POST['sexe'])){
 	    		$sexe = '';
 	    	}else if($_POST['sexe'] == 'tout'){
@@ -36,59 +57,67 @@
 	    	}else if($_POST['sexe'] == 'feminine'){
 	    		$sexe = 'feminine';
 	    	}
-	    	//对搜索条件的姓名进行处理
 	    	if($_POST['prenom'] == ''){
 	    		$prenom = '';
 	    	}else{
-	    		//此处应该是有防sql注入的处理的，就不写了
 	    		$prenom = $_POST['prenom'];
 	    	}
-	    	//提示用户上一步提交了哪些搜索条件
 	    	if($sexe!='' && $prenom!=''){
 	    		echo "<span>Vous avez cherche touts les auteurs qui s'appellent '".$prenom."'et de sexe'".$sexe."'</span>";
 	    		$sql = "select * from auteur where sexe = '".$sexe."' and prenom like '%".$prenom."%'";
-	    	}else if($sexe=='' && $prenom!=''){
-	    		echo "<span>Vous avez cherche touts les auteurs qui s'appellent '".$prenom."'</span>";
-	    		$sql = "select * from auteur where prenom like = '%".$prenom."%'";
-	    	}else if($sexe!='' && $prenom==''){
-	    		echo "<span>Vous avez cherche touts les auteurs qui s'appellent '".$sex."'</span>";
-	    		$sql = "select * from auteur where sexe = '".$sexe."'";
-	    	}else{
-	    		//这种情况在前端js判断不让他发送到这边来比较好-_-
-	    		echo "Il n'y a pas de condition de recherche";
-	    		$sql = "select * from auteur";
-	    	}
-		?>
-		
-	</div>
-	<div class="box">
-		<h1>clik !</h1>
-		<h1>clik !</h1>
-		<div class="text">
-			  <table border="1" width="400">
-			  <tr>
-			    <th width="150">N</th>
-			    <th>prenom</th>
-			    <th>sexe</th>
-				<th>choisir</th>
-			  </tr>
-		    <?php
-		    	require_once("lib/conn.php");
-				$sql = "select * from auteur where prenom = 'Akhil' ";
-		    	// echo $sql;这样可以看到sql语句
 				$result = mysql_query($sql);
 				while($row = mysql_fetch_assoc($result))
 				{
 					echo '<tr>';
-					echo '<td>'.$row['id'].'</td>';
-					echo '<td>'.$row['prenom'].'</td>';
-					echo '<td>'.$row['sexe'].'</td>';
-					echo "<td><a target='_self' href='edit_sex.php?id=".$row['id']."'>choisir le sexe</a></td>";
+					echo '<td class=k>'.$row['id'].'</td>';
+					echo '<td class=k>'.$row['prenom'].'</td>';
+					echo '<td class=k>'.$row['sexe'].'</td>';
+					echo "<td class=k><a target='_self' href='edit_sex.php?id=".$row['id']."'>choisir le sexe</a></td>";
 					echo '</tr>';
 				}
-		    ?>
-			</table>
-		</div>
+	    	}else if($sexe=='' && $prenom!=''){
+	    		echo "<span>Vous avez cherche touts les auteurs qui s'appellent '".$prenom."'</span>";
+	    		$sql = "select * from auteur where prenom like = '%".$prenom."%'";
+				$result = mysql_query($sql);
+				while($row = mysql_fetch_assoc($result))
+				{
+					echo '<tr>';
+					echo '<td class=k>'.$row['id'].'</td>';
+					echo '<td class=k>'.$row['prenom'].'</td>';
+					echo '<td class=k>'.$row['sexe'].'</td>';
+					echo "<td class=k><a target='_self' href='edit_sex.php?id=".$row['id']."'>choisir le sexe</a></td>";
+					echo '</tr>';
+				}
+	    	}else if($sexe!='' && $prenom==''){
+	    		echo "<span>Vous avez cherche touts les auteurs ".$sex."'</span>";
+	    		$sql = "select * from auteur where sexe = '".$sexe."'";
+				$result = mysql_query($sql);
+				while($row = mysql_fetch_assoc($result))
+				{
+					echo '<tr>';
+					echo '<td class=k>'.$row['id'].'</td>';
+					echo '<td class=k>'.$row['prenom'].'</td>';
+					echo '<td class=k>'.$row['sexe'].'</td>';
+					echo "<td class=k><a target='_self' href='edit_sex.php?id=".$row['id']."'>choisir le sexe</a></td>";
+					echo '</tr>';
+				}
+	    	}else{
+	    		//这种情况在前端js判断不让他发送到这边来比较好-_-
+	    		echo "Il n'y a pas de condition de recherche";
+	    		$sql = "select * from auteur";
+				$result = mysql_query($sql);
+				while($row = mysql_fetch_assoc($result))
+				{
+					echo '<tr>';
+					echo '<td class=k>'.$row['id'].'</td>';
+					echo '<td class=k>'.$row['prenom'].'</td>';
+					echo '<td class=k>'.$row['sexe'].'</td>';
+					echo "<td class=k><a target='_self' href='edit_sex.php?id=".$row['id']."'>choisir le sexe</a></td>";
+					echo '</tr>';
+				}
+	    	}
+		?>
+		</table>	
 	</div>
 </body>
 </html>
